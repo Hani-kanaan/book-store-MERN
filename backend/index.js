a45d768237287f1eee9ec6007ce02b3a78d0b25d
hani <hani.kanaan85@yahoo.com>
2024-09-27 17:22:59 +0300

    book model , create and get all

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { Book } from "./models/bookModel.js"; // Import the Book model
 
 dotenv.config();
 
@@ -8,9 +9,50 @@ export const PORT = process.env.PORT;
 export const mongoDBURL = process.env.MONGODB_URL;
 const app = express();
 
+//for parsing json body:
+app.use(express.json());
+
 app.get("/", (request, response) => {
   //console.log(request)
   return response.status(234).send("helloo");
+});
+// save new book:
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "missing field",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+//get all books
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
